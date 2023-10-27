0362a07dfc23d85562a23dd85a029f57878074b1
Zevind25 <zevind25@gmail.com>
2023-10-26 19:01:42 -0600

    disable jwt for backend testing
    in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,9 +145,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    #commented out for ease of backend testing
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
 }
 
 # Permissions:
