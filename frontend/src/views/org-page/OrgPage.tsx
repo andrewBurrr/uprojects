f1b1cb1de41838354baab0b174f7637354bff754
camden.warburton <camden.warburton@ucalgary.ca>
2023-11-25 14:05:09 -0700

    Just starting with org pages

--- frontend/src/views/org-page/OrgPage.tsx
+++ frontend/src/views/org-page/OrgPage.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useApi} from "contexts/ApiContext";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Grid } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Grid, Box, Typography, Button, Avatar } from '@mui/material';
 
 const OrgPage = () => {
     // const [data, setData] = useState({});
@@ -12,7 +12,37 @@ const OrgPage = () => {
     // },[]);
     return (
         <>
-            <h1>Hello from org page</h1>
+            <Box>
+            <Box>
+            <Grid container>
+                <Grid item padding={5} paddingBottom={1} >
+                    <Avatar alt="User Avatar" src="path/to/avatar.jpg"  sx={{ width: 120, height: 120 }} />
+                    <Typography variant="h5">Camy Cam's organization</Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                       Frontend Developer
+                    </Typography>
+                    <Button variant="outlined" size="medium">
+  Team
+</Button>
+<br></br>
+<Button variant="outlined" size="medium"  >
+  Orgs
+</Button>
+                </Grid>
+                <Grid item paddingTop={1}>
+                    <Typography variant="h5">
+                        About
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat.
+                    </Typography>
+                    
+
+
+                </Grid>
+            </Grid>
+        </Box>
+            </Box>
             
         </>
     );
