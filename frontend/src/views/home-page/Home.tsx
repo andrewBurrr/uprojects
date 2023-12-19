dc8fe8cebaf3d964ccd914de87499e6271ca42df
Andrew burton <andrew.burton@ucalgary.ca>
2023-12-18 22:37:32 -0700

    Update Home.tsx

--- frontend/src/views/home-page/Home.tsx
+++ frontend/src/views/home-page/Home.tsx
@@ -1,6 +1,4 @@
-import { Stack } from "@mui/material";
 import { Welcome } from "views/home-page/Welcome";
-import { Connect } from "views/home-page/Connect";
 import { Features } from "views/home-page/Features";
 import { Testimonials } from "views/home-page/Testimonials";
 
